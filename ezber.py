a3419eebb216d6b8765fde2278675e2be29b2b37
Bukalemun <65343320+malkemot47@users.noreply.github.com>
2020-12-29 01:33:12 +0300

    Add files via upload

---
+++ ezber.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+print("""
+HARFLER ~ YAZILIŞ ~ OKUNUŞ
+=<<<<<<<===>>>>>>===<><><><>=======
+A	  a	  Ey
+	
+B	  b	  Bi
+	
+C	  c	  Si
+	
+D	  d	  Di
+	
+E	  e	  İ
+	
+F	  f	  Ef
+	
+G	  g	  Ci
+	
+H	  h	  Eyç
+	
+i	  i	  Ay
+	
+j	  j	  Cey
+	
+K	  k	  Key
+	
+L	  l	  El
+	
+M	  m	  Em	
+
+N	  n	  En
+	
+O	  o	  O
+	
+P         p	  Pi
+	
+Q	  q	  Kiu
+	
+R	  r	  Ar
+	
+S	  s	  Es
+	
+T	  t	  Ti	
+
+U	  u	  Yu
+	
+V	  v	  Vi
+	
+W         w       Dabılyu
+	
+X	  x	  Ex
+	
+Y	  y	  Vay
+	
+Z	  z	  Zet
+
+""")
+
+print("""
+      ##########################################
+              Ezberledikten Sonra Pratik
+      Yapmak İçin [ingilizce.py]'yi Çalıştırın
+      ##########################################
+""")
