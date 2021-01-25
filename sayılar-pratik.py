586bdd5d2cd67a1f56be6dde77d2c71f7dc719e6
Bukalemun <65343320+malkemot47@users.noreply.github.com>
2021-01-25 04:03:44 +0300

    Add files via upload

---
+++ sayılar-pratik.py
@@ -0,0 +1,446 @@
+print("""         
+                  ========Bilgi=======
+Bilmediğiniz Soruya [pas] Deyip Geçebilirsiniz
+
+Yazdığınız Herşeyi Küçük Harflerle Yazınız.(aksi taktirde [YANLIŞ] hatası alırsınız)""")
+print("""___________________________________________""")
+ 
+print("\n""\n""SAYILAR""\t""\t""\t""\t""YAZILIŞI-OKUNUŞU")
+while True:
+	            a=input("\n""0""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="zero" and b=="zirou":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: zero-zirou")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: zero-zirou")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+	            	
+while True:
+	            a=input("\n""1""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="one" and b=="van":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: one-van")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: one-van")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+	            	
+while True:
+	            a=input("\n""2""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="two" and b=="tu":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: two-tu")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: two-tu")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+	            	
+while True:
+	            a=input("\n""3""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="three" and b=="tri":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: three-tri")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: three-tri")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+	            	
+while True:
+	            a=input("\n""4""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="four" and b=="for":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: four-for")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: four-for")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+	            	
+while True:
+	            a=input("\n""5""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="five" and b=="fayf":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: five-fayf")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: five-fayf")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""6""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="six" and b=="six":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: six-six")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: six-six")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+	            	
+while True:
+	            a=input("\n""7""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="seven" and b=="sevın":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: seven-sevın")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: seven-sevın")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""8""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="eight" and b=="eyt":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: eight-eyt")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: eight-eyt")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""9""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="nine" and b=="nayn":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: nine-nayn")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: nine-nayn")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""10""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="ten" and b=="ten":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: ten-ten")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: ten-ten")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+	            	
+while True:
+	            a=input("\n""11""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="eleven" and b=="elevın":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: eleven-elevın")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: eleven-elevın")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""12""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="twelve" and b=="tüvelf":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: twelve-tüvelf")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: twelve-tüvelf")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""13""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="thirteen" and b=="törtiin":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: thirteen-törtiin")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: thirteen-törtiin")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""14""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="fourteen" and b=="fortiin":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: fourteen-fortiin")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: fourteen-fortiin")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""15""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="fifteen" and b=="fiftiin":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: fifteen-fiftiin")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: fifteen-fiftiin")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""16""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="sixteen" and b=="sixtiin":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: sixteen-sixtiin")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: sixteen-sixtiin")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""17""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="seventeen" and b=="sevıntiin":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: seventeen-sevıntiin")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: seventeen-sevıntiin")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""18""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="eighteen" and b=="eytiin":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: eighteen-eytiin")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: eighteen-eytiin")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""19""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="nineteen" and b=="nayntiin":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: nineteen-nayntiin")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: nineteen-nayntiin")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""20""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="twenty" and b=="tüventiy":
+	            	print("\n""\n")
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: twenty-tüventiy""\n""\n")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: twenty-tüventiy""\n""\n")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+print("""         
+                  ========Bilgi=======
+Artık 20 den ve Sonrası [30, 40, 50, 60, 70, 80, 90] Çok Kolay {1}ler Basmağındakileri yazılışları ve okunuşlarını ezberledikten sonra yazılışlarını {twenty}'nin yanına aynen yazıyoruz.
+Mesala 21[twenty one] 22[twenty two] 23[twenty three] gördüğünüz gibi tamamen aynı.
+Okunuşlarıda tamamen aynı mesela 21[tüventiy van] 22[tüventiy tu] 23[tüventiy tri]gördüğünüz gibi tamamen aynı.
+şimdi 24, 25 ve 26 yı soracam bakalım bilecekmisiniz. 
+""")
+print("""___________________________________________""")
+
+while True:
+	            a=input("\n""24""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="twenty four" and b=="tüventy for":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: twenty four-tüventy for")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: twenty four-tüventy for")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""25""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="twenty five" and b=="tüventy fayf":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: twenty five-tüventy fayf")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: twenty five-tüventy fayf")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""26""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="twenty six" and b=="tüventy six":
+	            	print("\n""\n")
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: twenty six-tüventy six""\n""\n")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: twenty six-tüventy six""\n""\n")
+	            	break
+	            else:
+	              print("\t""\t""\t""\t""YANLIŞ")
+	            	
+print("""Gördüğünüz gibi Artık çok kolay Uzatmaya Gerek yok Dğerlerinden de 3 adet soracam""")
+
+while True:
+	            a=input("\n""30""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="thirty" and b=="törty":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: thirty-törty")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: thirty-törty")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""35""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="thirty five" and b=="törty fayf":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: thirty five-törty-fayf")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: thirty five-törty fayf")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+while True:
+	            a=input("\n""37""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="thirty seven" and b=="törty sevın":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: thirty seven-törty sevın")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: thirty seven-törty sevın")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
+	            	
+	            	
+while True:
+	            a=input("\n""39""\t""\t""\t""\t""Y.  :")
+	            b=input("\t""\t""\t""\t""O.  :")
+	            if a=="thirty nine" and b=="törty nayn":
+	            	print()
+	            	break
+	            elif a=="pas":
+	            	print("Malesef Doğru cvp: thirty nine-törty nayn")
+	            	break
+	            elif b=="pas":
+	            	print("Malesef Doğru cvp: thirty nine-törty nayn")
+	            	break
+	            else:
+	            	print("\t""\t""\t""\t""YANLIŞ")
