fc9818b3f1e6da505c8803a38a4e6af0a9e50d50
Bukalemun <65343320+malkemot47@users.noreply.github.com>
2021-01-25 16:41:31 +0300

    Add files via upload

---
+++ sayılar-ezber.py
@@ -0,0 +1,215 @@
+print("""
+
+
+S     Y       O
+---  ---     ---
+0 – zero (zirou)
+
+1 – one (van)
+
+2 – two (tu)
+
+3 – three (tri)
+
+4 – four (for)
+
+5 – five (fayf)
+
+6 – six (six)
+
+7 – seven (sevın)
+
+8 – eight (eyt)
+
+9 – nine (nayn)
+
+10 – ten (ten)
+
+11 – eleven (elevın)
+
+12 – twelve (tüvelf)
+
+13 – thirteen (törtiin)
+
+14 – fourteen (fortiin)
+
+15 – fifteen (fiftiin)
+
+16 – sixteen (sixtiin)
+
+17 – seventeen (sevıntiin)
+
+18 – eighteen (eytiin)
+
+19 – nineteen (nayntiin)
+
+20 – twenty (tüventi)
+
+21 – twenty-one (tüventi van)
+
+22 – twenty-two (tüventi tu)
+
+23 – twenty-three (tüventiy tırii)
+
+24 – twenty-four (tüventy for)
+
+25 – twenty-five (tüventy fayf)
+
+26 – twenty-six (tüventy six)
+
+27 – twenty-seven (tüventiy sevın)
+
+28 – twenty-eight (tüventiy eyt)
+
+29 – twenty-nine (tüventiy nayn)
+
+30 – thirty (törty)
+
+31 – thirty-one (törtiy van)
+
+32 – thirty-two (törtiy tu)
+
+33 – thirty-three (törtiy tıri)
+
+34 – thirty-four (törtiy for)
+
+35 – thirty-five (törty fayf)
+
+36 – thirty-six (törtiy siks)
+
+37 – thirty-seven (törty sevın)
+
+38 – thirty-eight (törtiy eyt)
+
+39 – thirty-nine (törty nayn)
+
+40 – forty (fortiy)
+
+41 – forty-one (fortiy van)
+
+42 – forty-two (fortiy tu)
+
+43 – forty-three (fortiy tıri)
+
+44 – forty-four (forty for)
+
+45 – forty-five (fortiy fayf)
+
+46 – forty-six (fortiy siks)
+
+47 – forty-seven (fortiy sevın)
+
+48 – forty-eight (fortiy eyt)
+
+49 – forty-nine (fortiy nayn)
+
+50 – fifty (fiftiy)
+
+51 – fifty-one (fifti van)
+
+52 – fifty-two (fifti tu)
+
+53 – fifty-three (fift,itıri)
+
+54 – fifty-four (fifti for)
+
+55 – fifty-five (fifti fayf)
+
+56 – fifty-six (fifti siks)
+
+57 – fifty-seven (fifti sevın)
+
+58 – fifty-eight (fifti eyt)
+
+59 – fifty-nine (fifti nayn)
+
+60 – sixty (siksti)
+
+61 – sixty-one (siksti van)
+
+62 – sixty-two (siksti tu)
+
+63 – sixty-three (siksti tıri)
+
+64 – sixty-four (siksti for)
+
+65 – sixty-five (siksti fayf)
+
+66 – sixty-six (siksti siks)
+
+67 – sixty-seven (siksti sevın)
+
+68 – sixty-eight (siksti eyt)
+
+69 – sixty-nine (siksti nayn)
+
+70 – seventy (sevınti)
+
+71 – seventy-one (sevınti van)
+
+72 – seventy-two (sevınti tu)
+
+73 – seventy-three (sevınti tıri)
+
+74 – seventy-four (sevınti for)
+
+75 – seventy-five (sevinti fayf)
+
+76 – seventy-six (sevınti siks)
+
+77 – seventy-seven (sevınti sevın)
+
+78 – seventy-eight (sevınti eyt)
+
+79 – seventy-nine (sevınti nayn)
+
+80 – eighty (eyti)
+
+81 – eighty-one (eyti van)
+
+82 – eighty-two (eyti tu)
+
+83 – eighty-three (eyti tıri)
+
+84 – eighty-four (eyti for)
+
+85 – eighty-five (eyti fayf)
+
+86 – eighty-six (eyti siks)
+
+87 – eighty-seven (eyti sevın)
+
+88 – eighty-eight (eyti eyt)
+
+89 – eighty-nine (eyti nayn)
+
+90 – ninety (naynti)
+
+91 – ninety-one (naynti van)
+
+92 – ninety-two (naynti tu)
+
+93 – ninety-three (naynti tıri)
+
+94 – ninety-four (naynti for)
+
+95 – ninety-five (naynti fayf)
+
+96 – ninety-six (naynti siks)
+
+97 – ninety-seven (naynti sevın)
+
+98 – ninety-eight (naynti eyt)
+
+99 – ninety-nine (naynti nayn)
+
+100 – one hundred (van handırıd)
+
+""")
+
+print("""
+      ##########################################
+              Ezberledikten Sonra Pratik
+      Yapmak İçin [sayılar-pratik.py]'yi Çalıştırın
+      ##########################################
+""")
